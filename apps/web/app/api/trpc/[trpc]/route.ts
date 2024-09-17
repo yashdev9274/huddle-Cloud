b1a04e40e1158aa44f9f4190d755a4cd1b60332e
yashdev9274 <yashdev.yvd@gmail.com>
2024-09-17 18:22:31 +0530

    update trpc.

---
+++ apps/web/app/api/trpc/[trpc]/route.ts
@@ -0,0 +1,14 @@
+// import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
+// import { appRouter } from "../../../../trpc/index"
+
+
+// function handler(req: Request) {
+//     return fetchRequestHandler({
+//         endpoint: '/api/trpc',
+//         req,
+//         router: appRouter,
+//         createContext: () => ({ ... })
+//     });
+// }
+
+// export { handler as GET, handler as POST };
--- apps/web/app/components/Providers.tsx
+++ apps/web/app/components/Providers.tsx
@@ -1,11 +1,28 @@
 'use client'
 
-import { QueryClient } from "@tanstack/react-query"
-import { useState } from "react"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { httpBatchLink } from "@trpc/client"
+import { trpc } from "app/_trpc/client"
+import { PropsWithChildren, useState } from "react"
 
-const Providers = () => {
+const Providers = ({ children }: PropsWithChildren) => {
     const [queryClient] = useState(() => new QueryClient())
-    // const [trpcClient] = useState(() =>)
+    const [trpcClient] = useState(() => trpc.createClient({
+        links: [
+            httpBatchLink({
+                url: 'http://localhost:300/api/trpc',
+            })
+        ]
+    }))
+
+
+    return (
+        <trpc.Provider client={trpcClient} queryClient={queryClient}>
+            <QueryClientProvider client={queryClient}>
+                {children}
+            </QueryClientProvider>
+        </trpc.Provider>
+    )
 }
 
 export default Providers
