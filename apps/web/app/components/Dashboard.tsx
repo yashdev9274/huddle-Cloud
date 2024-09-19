dbc0d381e5e18105361f3119f086139939712ab8
yashdev9274 <yashdev.yvd@gmail.com>
2024-09-19 18:24:49 +0530

    update: dashboard, update file listing feature.

--- apps/web/app/components/Dashboard.tsx
+++ apps/web/app/components/Dashboard.tsx
@@ -4,6 +4,7 @@ import React from 'react'
 import UploadButton from './UploadButton'
 import { trpc } from 'app/_trpc/client'
 import { Ghost } from 'lucide-react'
+import Link from 'next/link'
 
 function Dashboard() {
 
@@ -22,7 +23,31 @@ function Dashboard() {
             {/* display Users Files */}
 
             {Files && Files?.length !== 0 ? (
-                <div></div>
+                <ul className='mt-8 grid grid-cols-1 gap-6 divide-y divide-zinc-200 md:grid-cols-2 lg:grid-cols-3'>
+                    {Files.sort((a, b) => {
+                        return new Date(b.uploadDate).getTime() - new Date(a.uploadDate).getTime()
+                    }).map((Files) => (
+                        <li
+                            key={Files.id}
+                            className='col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow transition hover:shadow-lg'>
+                            <Link
+                                href={`/dashboard/${Files.id}`}
+                                className='flex flex-col gap-2'>
+                                <div className='pt-6 px-6 flex w-full items-center justify-between space-x-6'>
+                                    <div className='h-10 w-10 flex-shrink-0 rounded-full bg-gradient-to-r from-cyan-500 to-blue-500' />
+                                    <div className='flex-1 truncate'>
+                                        <div className='flex items-center space-x-3'>
+                                            <h3 className='truncate text-lg font-medium text-zinc-900'>
+                                                {Files.filename}
+                                            </h3>
+                                        </div>
+                                    </div>
+                                </div>
+                            </Link>
+
+                        </li>
+                    ))}
+                </ul>
             ) : isLoading ? (
                 <div></div>
             ) : (
