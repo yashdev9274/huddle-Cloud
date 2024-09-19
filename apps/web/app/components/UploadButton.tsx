3e687727ba9e9aabc71fa4783428f1f7dd10c571
yashdev9274 <yashdev.yvd@gmail.com>
2024-09-19 12:47:02 +0530

    update dashboard component, update uploadButton and its feature.

--- apps/web/app/components/UploadButton.tsx
+++ apps/web/app/components/UploadButton.tsx
@@ -1,10 +1,27 @@
-import { Dialog } from '@/components/ui/dialog'
-import { Button } from '@repo/ui/button'
-import React from 'react'
+'use client'
+
+import { Dialog, DialogTrigger } from '@/components/ui/dialog'
+// import { Button } from '@/components/ui/button'
+import React, { useState } from 'react'
+import { Button } from '../../../web/src/components/ui/button'
 
 function UploadButton() {
+
+    const [isOpen, setIsOpen] = useState<boolean>(false)
     return (
-        <Dialog>UploadButton</Dialog>
+        <Dialog
+            open={isOpen}
+            onOpenChange={(v) => {
+                if (!v) {
+                    setIsOpen(v)
+                }
+            }}
+        >
+            <DialogTrigger asChild>
+                <Button className='bg-black'> Upload File</Button>
+
+            </DialogTrigger>
+        </Dialog>
     )
 }
 
